ce982ca935677e1ab3d7fe0cf4218a9d5c71ceb2
Mariley20 <mariley2018@gmail.com>
2017-11-29 22:57:33 -0500

    work

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import createStore from "redux-zero";
-let js = [
+let work = [
     {
         name: 'FotoLog',
         mode: 'Individual',
@@ -7,47 +7,86 @@ let js = [
                 'atillos de Sushi',
         tecnologies: ['HTML',' CSS', 'jQuery', 'Programacion Orientada a Objetos', 'Web Storage Api'],
         img: 'img/fotolog.png',
-        github: ['https://github.com/Mariley20/comentarios-fotolog-II', 'https://github.com/Mariley20/comentarios-fotolog/'],
-        website: ['https://mariley20.github.io/comentarios-fotolog-II/', 'https://mariley20.github.io/comentarios-fotolog/']
-    }
-]
-let work = [
+        github: 'https://github.com/Mariley20/comentarios-fotolog-II',
+        website: 'https://mariley20.github.io/comentarios-fotolog-II/'
+    },
     {
-        name: 'japanese Food',
+        name: 'Game Planet',
         mode: 'Colaborativo',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
                 'atillos de Sushi',
-        tecnologies: 'React, js, Redux, HTML, CSS, Bootstrap 3',
-        img: 'img/japaneseFood.png',
-        github: 'https://github.com/Mariley20/Japanesefood',
-        website: 'https://jani-123.github.io/Japanesefood/'
+        tecnologies: ['HTML',' BootStrap', 'jQuery'],
+        img: 'img/gameplanet.png',
+        github: 'https://github.com/Mariley20/game-planet',
+        website: 'https://mariley20.github.io/game-planet/'
+    },
+    {
+        name: 'Save the Koala-II',
+        mode: 'Individual',
+        description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
+                'atillos de Sushi',
+        tecnologies: ['HTML', 'Bootstrap','jQuery'],
+        img: 'img/savekoala.png',
+        github: 'https://github.com/Mariley20/save-koala-II',
+        website: 'https://mariley20.github.io/save-koala-II/'
+    },
+    {
+        name: 'Lab Studio',
+        mode: 'Individual',
+        description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
+                'atillos de Sushi',
+        tecnologies: ['HTML', 'CSS', 'Bootstrap', 'jQuery'],
+        img: 'img/labstudio.png',
+        github: 'https://github.com/Mariley20/lab-Studio',
+        website: 'https://mariley20.github.io/lab-Studio/'
     }, {
-        name: 'japanese Food',
+        name: 'Lyft Card Bloques',
         mode: 'Colaborativo',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
                 'atillos de Sushi',
-        tecnologies: 'React, js, Redux, HTML, CSS, Bootstrap 3',
-        img: 'img/japaneseFood.png',
-        github: 'https://github.com/Mariley20/Japanesefood',
-        website: 'https://jani-123.github.io/Japanesefood/'
+        tecnologies: ['HTML', 'Materialize','jQuery'],
+        img: 'img/lyft.png',
+        github: 'https://github.com/Mariley20/proyecto-final-bloques',
+        website: 'https://mariley20.github.io/proyecto-final-bloques/'
     }, {
-        name: 'japanese Food',
-        mode: 'Colaborativo',
+        name: 'Freelance BootStrap',
+        mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
                 'atillos de Sushi',
-        tecnologies: 'React, js, Redux, HTML, CSS, Bootstrap 3',
-        img: 'img/japaneseFood.png',
-        github: 'https://github.com/Mariley20/Japanesefood',
-        website: 'https://jani-123.github.io/Japanesefood/'
-    }, {
-        name: 'japanese Food',
-        mode: 'Colaborativo',
+        tecnologies: ['Bootstrap','HTML', 'jQuery'],
+        img: 'img/freelance.png',
+        github: 'https://github.com/Mariley20/FreeLancer-flexbox',
+        website: 'https://mariley20.github.io/FreeLancer-flexbox/'
+    },
+    {
+        name: 'To Do List',
+        mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
                 'atillos de Sushi',
-        tecnologies: 'React, js, Redux, HTML, CSS, Bootstrap 3',
-        img: 'img/japaneseFood.png',
-        github: 'https://github.com/Mariley20/Japanesefood',
-        website: 'https://jani-123.github.io/Japanesefood/'
+        tecnologies: ['Bootstrap','HTML', 'jQuery', 'Programación Orientada a Objetos'],
+        img: 'img/todolist.png',
+        github: 'https://github.com/Mariley20/listaTareasPOO',
+        website: 'https://mariley20.github.io/listaTareasPOO/'
+    },
+    {
+        name: 'App Love',
+        mode: 'Individual',
+        description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
+                'atillos de Sushi',
+        tecnologies: ['Bootstrap','HTML', 'jQuery'],
+        img: 'img/applove.png',
+        github: 'https://github.com/Mariley20/applove-II',
+        website: 'https://mariley20.github.io/applove-II/'
+    },
+    {
+        name: 'Panda Love',
+        mode: 'Individual',
+        description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
+                'atillos de Sushi',
+        tecnologies: ['CSS','HTML', 'jQuery'],
+        img: 'img/pandalove.png',
+        github: 'https://github.com/Mariley20/PandaPanda',
+        website: 'https://mariley20.github.io/PandaPanda/'
     }, {
         name: 'japanese Food',
         mode: 'Colaborativo',
