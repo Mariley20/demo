b3b31ae3b609bdf6b95d9bfc8dd1b7d2e6b48c19
Mariley20 <mariley2018@gmail.com>
2017-12-04 10:58:55 -0500

    store imgsrc

--- src/store.js
+++ src/store.js
@@ -1,21 +1,20 @@
 import createStore from "redux-zero";
-let workObj = [
-    {
+let workObj = [{
         name: 'Game Planet',
         mode: 'Colaborativo',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
-        tecnologies: ['HTML',' BootStrap', 'jQuery'],
+            'atillos de Sushi',
+        tecnologies: ['HTML', ' BootStrap', 'jQuery'],
         img: 'img/gameplanet.png',
         github: 'https://github.com/Mariley20/game-planet',
         website: 'https://mariley20.github.io/game-planet/'
-    },    
+    },
     {
         name: 'App Love',
         mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
-        tecnologies: ['Bootstrap','HTML', 'jQuery'],
+            'atillos de Sushi',
+        tecnologies: ['Bootstrap', 'HTML', 'jQuery'],
         img: 'img/applove.png',
         github: 'https://github.com/Mariley20/applove-II',
         website: 'https://mariley20.github.io/applove-II/'
@@ -24,19 +23,19 @@ let workObj = [
         name: 'Google Maps',
         mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
-        tecnologies: ['CSS','HTML', 'jQuery', 'GoogleMaps Api', 'Programación Orientada A Objetos'],
+            'atillos de Sushi',
+        tecnologies: ['CSS', 'HTML', 'jQuery', 'GoogleMaps Api', 'Programación Orientada A Objetos'],
         img: 'img/googlemap.png',
         github: 'https://github.com/Mariley20/easy-vreco',
         website: 'https://mariley20.github.io/easy-vreco/'
     },
-     {
+    {
         name: 'japanese Food',
         mode: 'Colaborativo',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
+            'atillos de Sushi',
         tecnologies: ['React', 'js', 'Redux', 'HTML', 'CSS', 'Bootstrap', 'Programación por Componentes'],
-        img: 'img/japaneseFood.png',
+        img: 'img/japaneseFood.PNG',
         github: 'https://github.com/Mariley20/Japanesefood',
         website: 'https://jani-123.github.io/Japanesefood/'
     },
@@ -44,9 +43,9 @@ let workObj = [
         name: 'Trello',
         mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
+            'atillos de Sushi',
         tecnologies: ['React', 'js', 'Redux', 'Router', 'HTML', 'CSS', 'Bootstrap', 'Programación por Componentes'],
-        img: 'img/japaneseFood.png',
+        img: 'img/trello.png',
         github: 'https://github.com/Mariley20/trello-firebase-redux/',
         website: 'https://mariley20.github.io/trello-firebase-redux/'
     },
@@ -54,7 +53,7 @@ let workObj = [
         name: 'Registro de Invitados',
         mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
+            'atillos de Sushi',
         tecnologies: ['Reactjs', 'Redux', 'Router', 'HTML', 'Bootstrap', 'Programación por Componentes'],
         img: 'img/invitados.png',
         github: 'https://github.com/Mariley20/Registro-invitados-mvc/',
@@ -64,8 +63,8 @@ let workObj = [
         name: 'ScoreBoard',
         mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
-        tecnologies: ['Reactjs','CSS', 'Redux', 'Router', 'HTML', 'Programación por Componentes'],
+            'atillos de Sushi',
+        tecnologies: ['Reactjs', 'CSS', 'Redux', 'Router', 'HTML', 'Programación por Componentes'],
         img: 'img/scoreboard.png',
         github: 'https://github.com/Mariley20/Registro-invitados-mvc/',
         website: 'https://mariley20.github.io/Registro-invitados-mvc/'
@@ -74,8 +73,8 @@ let workObj = [
         name: 'Reserva de Pasajes',
         mode: 'Individual',
         description: 'Una web dedica a la comida Japonesa, donde se da a conocer los más deliciosos pl' +
-                'atillos de Sushi',
-        tecnologies: ['HTML', 'CSS', 'Reactjs','Redux', 'Router', 'Programación por Componentes'],
+            'atillos de Sushi',
+        tecnologies: ['HTML', 'CSS', 'Reactjs', 'Redux', 'Router', 'Programación por Componentes'],
         img: 'img/reservapasajes.png',
         github: 'https://github.com/Mariley20/Reserva_de_Pasajes/',
         website: 'https://mariley20.github.io/Reserva_de_Pasajes/'
