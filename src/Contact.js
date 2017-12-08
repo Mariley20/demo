b4a8eac185546f660f8696a2125244471b98ecd1
gmakersteam <gmakers.team@gmail.com>
2017-12-08 16:31:56 -0500

    map

--- src/Contact.js
+++ src/Contact.js
@@ -26,12 +26,12 @@ const FormContact = () => {
             {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7098.94326104394!2d78.0430654485247!3d27.172909818538997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1385710909804" width="600" height="450" frameborder="0" style={{border:0}}></iframe> */}
             <ul className='informationPersonal'>
                 <li className="floating-box">
-                    <i class="fa fa-envelope-o" aria-hidden="true"></i> <br/>
-                    <img className='email-me' src='img/email.png' />
+                    <i className="fa fa-envelope-o" aria-hidden="true"></i> <br/>
+                    <h3>mariley2018 @ gmail.com</h3>
                 </li>
                 <li className="floating-box">
                     <img className='flag-marke' src='img/peru.svg' />
-                    <h3>Arequipa,Peru</h3>
+                    <h3>Arequipa, Peru</h3>
                 </li>
                 
             </ul>
